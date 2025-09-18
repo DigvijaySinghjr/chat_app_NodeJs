f4ec63e36376b95ee6f6fda8fcfe532c0f58da59
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-09-18 15:12:20 +0530

    render dynamic chat list in views/index.ejs  & Db setup
    Set up GET routes to test EJS server-side rendering
    
    mongoose configuration
    
    Added chat model

--- index.js
+++ index.js
@@ -2,25 +2,37 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 
+const connect = require('./config/database-config');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
  
 io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
+    // console.log('a user connected', socket.id);
 
-    socket.on('msg_send', (data) => {                              //listening for data to recieve
-        console.log(data);
-        //io.emit('msg_rcvd', data);                                  // send to all 
-        //socket.emit('msg_rcvd', data)                               // send to server
-        socket.broadcast.emit('msg_rcvd', data)                        // send to room or group
-    })
+    // socket.on('msg_send', (data) => {                              //listening for data to recieve
+    //     console.log(data);
+    //     //io.emit('msg_rcvd', data);                                  // send to all 
+    //     //socket.emit('msg_rcvd', data)                               // send to server
+    //     socket.broadcast.emit('msg_rcvd', data)                        // send to room or group
+    // })
 
-   
+
+
 });
-
+app.set('view engine',  'ejs');
 app.use('/', express.static(__dirname + '/public'));
 
-server.listen(3000, () => {               //generally we listen on app, like app.listen()
-    console.log('Server started')
+app.get('/chat/:roomid', (req, res) => {
+    res.render('index', {
+        name: "Sanket",
+        id: req.params.roomid
+    });
+})
+
+server.listen(3000, async () => {               //generally we listen on app, like app.listen()
+    console.log('Server started');
+    await connect();
+    console.log('mongo db connected');
 });
